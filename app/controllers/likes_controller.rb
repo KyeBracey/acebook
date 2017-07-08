9ce6fecd4dfd63bc281476524480386d7dee1d0a
Kynosaur <kye.bracey@gmail.com>
2017-07-08 21:56:11 +0100

    Updates like model and posts index view so that users can like more than one different post (but can still only like each post once).  Also adds simplecov

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,14 @@
 class LikesController < ApplicationController
   def create
-    @post = Post.find(params[:post_id])
-    @like = @post.likes.build(like_params)
-    @like.user = current_user
-    @like.save
+    post = Post.find(params[:post_id])
+    like = post.likes.build(like_params)
+    like.user = current_user
+    like.save
     redirect_to posts_path
   end
 
   def new
-    @like = Like.new
+
   end
 
   def like_params
